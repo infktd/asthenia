49ad1a01882913aa6586a036ff846d11e194bd01
infktd <25016104+infktd@users.noreply.github.com>
2025-10-06 21:08:39 -0500

    Update .gitignore and modify user in home-manager.nix

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -9,18 +9,13 @@ let user = "jayne"; in
   ];
 
   nix = {
+    enable = false;  # Disable nix-darwin's Nix management to avoid conflict with Determinate
     package = pkgs.nix;
 
     settings = {
       trusted-users = [ "@admin" "${user}" ];
       substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-    };
-
-    gc = {
-      automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 30d";
     };
 
     extraOptions = ''
--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  user = "jayne";
+  user = "infktd";
   xdg_configHome  = "/home/${user}/.config";
   shared-programs = import ../shared/home-manager.nix { inherit config pkgs lib; };
   shared-files = import ../shared/files.nix { inherit config pkgs; };
