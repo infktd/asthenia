e6ecede0ae78ab6551542c0c585a1651e7b7a097
infktd <25016104+infktd@users.noreply.github.com>
2026-01-09 08:51:59 -0600

    feat: add new configurations for waybar, waypaper, and fish; enhance niri window management

--- home/wm/niri/default.nix
+++ home/wm/niri/default.nix
@@ -44,6 +44,9 @@ in
     ../../programs/vscode/vscode.nix
     ../../programs/discord/discord.nix
     ../../programs/obsidian/obsidian.nix
+    ../../programs/discord/waybar.nix
+    ../../programs/waypaper/waypaper.nix
+    ../../programs/yazi/yazi.nix
   ];
 
   home = {
